c81d3bc7cfd751f0e9423be91246241a287ee329
RobertoCarrichi <robertocarrichiunam@gmail.com>
2020-10-29 21:11:51 -0600

    Añadí el propotivo de la lectura de datos.

---
+++ EDA2 Proyecto 1 Eq9 G9/Alumno.java
@@ -0,0 +1,56 @@
+public class Alumno {
+    
+    private String nombre;
+    private String apellido;
+    private String noCuenta;
+    
+    public Alumno( String nombre, String apellido, String noCuenta){
+    
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.noCuenta = noCuenta;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the noCuenta
+     */
+    public String getNoCuenta() {
+        return noCuenta;
+    }
+
+    /**
+     * @param noCuenta the NoCuenta to set
+     */
+    public void setNoCuenta(String noCuenta) {
+        this.noCuenta = noCuenta;
+    }
+  
+}
