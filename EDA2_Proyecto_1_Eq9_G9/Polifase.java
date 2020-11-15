2916467a5c8865656ba3fac8bd69fae0f51cd3b0
MirandaBueno <54423136+MirandaBueno@users.noreply.github.com>
2020-11-15 10:23:38 -0600

    Update Polifase.java

--- EDA2_Proyecto_1_Eq9_G9/Polifase.java
+++ EDA2_Proyecto_1_Eq9_G9/Polifase.java
@@ -1,306 +1,220 @@
+
 // Bibliotecas de apoyo
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Arrays;
 import java.io.FileWriter;
-import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Polifase{
-	public void sort(ArrayList<Alumno> listadatos){
+	// listadatos es la lista de los objetos tipo Alumno, con los atributos nombre, apellido y num de cuenta, que se leyeron del archivo original
+	//nombre es el nombre del archivo original
+	public void sort(ArrayList<Alumno> listadatos,String nombre,char opc){
 		
-		// Arreglo que almacena de forma temporal los bloques de n=7 para ordenarlos con Algoritmo de Ordenamiento Interno
-		ArrayList<Alumno> AOI = new ArrayList<Alumno>(); 
+		// Arreglo que almacena de forma temporal los bloques de n=7 
+		// y los ordena con un Algoritmo de Ordenamiento Interno
+		ArrayList<Alumno> AOI = new ArrayList<>(); 
 		
-		// Los archivos seran listas
-		ArrayList<Alumno> f0 = new ArrayList<Alumno>(); 
-		ArrayList<Alumno> f1 = new ArrayList<Alumno>(); 
-		ArrayList<Alumno> f2 = new ArrayList<Alumno>(); 
-		ArrayList<Alumno> f3 = new ArrayList<Alumno>();
+		// Los archivos auxiliares seran listas
+		ArrayList<Alumno> f0 = new ArrayList<>(); 
+		ArrayList<Alumno> f1 = new ArrayList<>(); 
+		ArrayList<Alumno> f2 = new ArrayList<>(); 
+		ArrayList<Alumno> f3 = new ArrayList<>();
+		Scanner scan=new Scanner(System.in);
 		
 		try{
-			FileWriter iteraciones=new FileWriter("./iteraciones.txt");
+			
+			// en iteraciones se guardan las fases 1 y 2, y sus iteraciones
+			FileWriter iteraciones;
+			if(opc=='2'){
+				iteraciones=new FileWriter("./iteracionesPolifase_Apellido_"+nombre+".txt");
+				System.out.println("\n\t\t\t****FIN****\n\nLas iteraciones se encuentran en la carpeta actual, en el archivo: \n\titeracionesPolifase_Apellido_"+nombre+".txt");
+			}else if(opc=='1'){
+				iteraciones=new FileWriter("./iteracionesPolifase_Nombre_"+nombre+".txt");
+				System.out.println("\n\t\t\t****FIN****\n\nLas iteraciones se encuentran en la carpeta actual, en el archivo: \n\titeracionesPolifase_Nombre_"+nombre+".txt");
+				}else
+					iteraciones=new FileWriter("./ERROR.txt");
 			f0=listadatos;
-			int i=0,k,n=7,j=0;
-			Alumno index;
-			iteraciones.write("***Fase 1***\n");
-		//fase 1, leer archivo original "f0", y dividir en bloques de n=7, ordenar cada bloque y colocar uno y uno en f1 y f2
-		
+			//  n es el tamaño de los bloques
+			int n=7;
+			iteraciones.write("***FASE 1***\n");
+		  //******FASE 1*****
+		  //leer archivo original "f0", y dividir en bloques de n=7, ordenar cada bloque y colocar uno a uno en f1 y f2
 			while(f0.isEmpty()!=true){
-				i=0;
-				while(f0.isEmpty()!=true&&i<n){
-					//1   AOI[i]=f0.remove(0);
-					AOI.add(f0.remove(0));
-					//1   index=AOI[i]; 
-					
-			//****algoritmo de oredenamiento interno que se usa es insertion sort****	
-					index=AOI.get(i);
-					k=i-1;
-					//1   while(k>=0&&AOI[k].getNombre().compareTo(index.getNombre())>0){
-					
-					while(k>-1&&(AOI.get(k).getNombre().compareTo(index.getNombre()))>0){
-						//1   AOI[k+1]=AOI[k];
-						AOI.set(k+1,AOI.get(k));
-						//System.out.println(AOI.get(k+1).getNombre()+","+AOI.get(k+1).getApellido()+","+AOI.get(k+1).getNoCuenta());
-						k--;
-					}
-					//1   AOI[k+1]=index;
-					AOI.set(k+1,index);
-					i++;
-				}
+				insertionSort(f0,AOI,n,opc);
 				iteraciones.write("F1\n");
-			
-				//1   for(int alu=0;alu<i;alu++){
+				// colocar bloque ordenado en lista f1
 				for(Alumno alu:AOI){
-					//1  f1.add(AOI[alu]);
 					f1.add(alu);
-					//1  iteraciones.write(AOI[alu].getNombre()+", "+AOI[alu].getApellido()+", "+AOI[alu].getNoCuenta()+"\n");
 					iteraciones.write(alu.getNombre()+", "+alu.getApellido()+", "+alu.getNoCuenta()+"\n");
 				}
-				//1   Arrays.fill(AOI,null);
 				AOI.clear();
-				i=0;
-				while(f0.isEmpty()!=true&&i<n){
-					//1 AOI[i]=f0.remove(0);
-					AOI.add(f0.remove(0));
-					//1 index=AOI[i];
-					index=AOI.get(i);
-					k=i-1;
-					//1 while(k>=0&&AOI[k].getNombre().compareTo(index.getNombre())>0){
-					// Si >0 entonces index va antes
-					while(k>-1&&AOI.get(k).getNombre().compareTo(index.getNombre())>0){
-						//1 AOI[k+1]=AOI[k];
-						AOI.set(k+1,AOI.get(k));
-						k=k-1;
-					}
-					//1 AOI[k+1]=index;
-					AOI.set(k+1,index);
-					i++;
-				}
+				insertionSort(f0,AOI,n,opc);
 				iteraciones.write("F2\n");
-			
-				//for(int alu=0;alu<i;alu++){
 				for(Alumno alu:AOI){
-					//f2.add(AOI[alu]);
 					f2.add(alu);
-					//iteraciones.write(AOI[alu].getNombre()+", "+AOI[alu].getApellido()+", "+AOI[alu].getNoCuenta()+"\n");
 					iteraciones.write(alu.getNombre()+", "+alu.getApellido()+", "+alu.getNoCuenta()+"\n");
 				}
-				//Arrays.fill(AOI,null);
 				AOI.clear();
-			}//fin fase 1
-			iteraciones.write("\nFase 1/F1\n\n");
-			for(Alumno alu:f1){
-				iteraciones.write(alu.getNombre()+", "+alu.getApellido()+", "+alu.getNoCuenta()+"\n");
-			}
-			iteraciones.write("\nFase 1/F2\n\n");
-			for(Alumno alu:f2){
-				iteraciones.write(alu.getNombre()+", "+alu.getApellido()+", "+alu.getNoCuenta()+"\n");
-			}
-		
-			iteraciones.write("\n\n***Fase 2***\n");
-		while((f1.isEmpty()!=true&&f2.isEmpty()!=true)||(f0.isEmpty()!=true&&f3.isEmpty()!=true)){
-			iteraciones.write("\nF1-F2 --> F0-F3\n");
-			while(f1.isEmpty()!=true||f2.isEmpty()!=true){
-				i=0;
-				j=0;
-				while((f1.isEmpty()!=true||f2.isEmpty()!=true)&&(i<n||j<n)){
-					if(f1.isEmpty()!=true&&f2.isEmpty()!=true&&i<n&&j<n){
-						Alumno F1=f1.get(0);
-						Alumno F2=f2.get(0);
-						if(F1.getNombre().compareTo(F2.getNombre())>0){
-							f0.add(f2.remove(0));
-							j++;
-						}else{
-							f0.add(f1.remove(0));
-							i++;
-						}
-					}else if((f1.isEmpty()==true)||(i>=n)){
-						f0.add(f2.remove(0));
-						j++;
-					}else{
-						f0.add(f1.remove(0));
-						i++;
-					}
-					
-				}
-				iteraciones.write("\nF0\n");
-				for(Alumno alu:f0){
-					iteraciones.write(alu.getNombre()+", "+alu.getApellido()+", "+alu.getNoCuenta()+"\n");
-				}
-				i=0;
-				j=0;
-				while((f1.isEmpty()!=true||f2.isEmpty()!=true)&&(i<n||j<n)){
-					if(f1.isEmpty()!=true&&f2.isEmpty()!=true&&i<n&&j<n){
-						Alumno F1=f1.get(0);
-						Alumno F2=f2.get(0);
-						if(F1.getNombre().compareTo(F2.getNombre())>0){
-							f3.add(f2.remove(0));
-							j++;
-						}else{
-							f3.add(f1.remove(0));
-							i++;
-						}
-					}else if(f1.isEmpty()==true||(i>=n)){
-						f3.add(f2.remove(0));
-						j++;
-					}else{
-						f3.add(f1.remove(0));
-						i++;
-					}
-				}
-				iteraciones.write("\nF3\n");
-				for(Alumno alu:f3){
-					iteraciones.write(alu.getNombre()+", "+alu.getApellido()+", "+alu.getNoCuenta()+"\n");
-				}
-			}
-			n*=2;
-			iteraciones.write("\nF0-F3 --> F1-F2\n");
-			//pasar a f1 y f2 de f0 y f3
-			while(f0.isEmpty()!=true||f3.isEmpty()!=true){
-				i=0;
-				j=0;
-				
-				//f1
-				while((f0.isEmpty()!=true||f3.isEmpty()!=true)&&(i<n||j<n)){
-					if(f0.isEmpty()!=true&&f3.isEmpty()!=true&&i<n&&j<n){
-						Alumno F0=f0.get(0);
-						Alumno F3=f3.get(0);
-						if(F0.getNombre().compareTo(F3.getNombre())>0){
-							f1.add(f3.remove(0));
-							j++;
-						}else{
-							f1.add(f0.remove(0));
-							i++;
-						}
-					}else if((f0.isEmpty()==true)||(i>=n)){
-						f1.add(f3.remove(0));
-						j++;
-					}else{
-						f1.add(f0.remove(0));
-						i++;
-					}
-					
-				}
-				
-				iteraciones.write("\nF1\n");
-				for(Alumno alu:f1){
-					iteraciones.write(alu.getNombre()+", "+alu.getApellido()+", "+alu.getNoCuenta()+"\n");
-				}
-				i=0;
-				j=0;
-				while((f0.isEmpty()!=true||f3.isEmpty()!=true)&&(i<n||j<n)){
-					if(f0.isEmpty()!=true&&f3.isEmpty()!=true&&i<n&&j<n){
-						Alumno F0=f0.get(0);
-						Alumno F3=f3.get(0);
-						if(F0.getNombre().compareTo(F3.getNombre())>0){
-							f2.add(f3.remove(0));
-							j++;
-						}else{
-							f2.add(f0.remove(0));
-							i++;
-						}
-					}else if(f0.isEmpty()==true||(i>=n)){
-						f2.add(f3.remove(0));
-						j++;
-					}else{
-						f2.add(f0.remove(0));
-						i++;
-					}
-					
-				}
-				iteraciones.write("\nF2\n");
-				for(Alumno alu:f2){
-					iteraciones.write(alu.getNombre()+", "+alu.getApellido()+", "+alu.getNoCuenta()+"\n");
-				}
+			} //fin fase 1
 			
-			}
-			n*=2;
-		}
-		
-		try{
-			FileWriter archivoFinal=new FileWriter("./archivoFinal.txt");
-			archivoFinal.write("\t***Polifase***\n");
-			if(f0.isEmpty()!=true){
-				for(Alumno alu:f0){
-					archivoFinal.write(alu.getNombre()+", "+alu.getApellido()+", "+alu.getNoCuenta()+"\n");
+			iteraciones.write("\nFase 1/F1\n\n");
+			imprimirDocumento(f1,iteraciones);
+	
+			iteraciones.write("\nFase 1/F2\n\n");
+			imprimirDocumento(f2,iteraciones);
+			
+		//*****FASE 2*******
+			iteraciones.write("\n\n***FASE 2***\n");
+			// vaciar f1 y f2 en f0 y f3, o al reves
+			while((f1.isEmpty()!=true&&f2.isEmpty()!=true)||(f0.isEmpty()!=true&&f3.isEmpty()!=true)){
+				iteraciones.write("\nF1-F2 --> F0-F3\n");
+				// pasae a f0 y f3 claves de f1 y f2
+				while(f1.isEmpty()!=true||f2.isEmpty()!=true){
+					faseDos(f0,f1,f2,n,opc);
+					iteraciones.write("\nF0\n");
+					imprimirDocumento(f0,iteraciones);
+					faseDos(f3,f1,f2,n,opc);
+					iteraciones.write("\nF3\n");
+					imprimirDocumento(f3,iteraciones);
 				}
-			}else{
-				for(Alumno alu:f1){
-					archivoFinal.write(alu.getNombre()+", "+alu.getApellido()+", "+alu.getNoCuenta()+"\n");
+				n*=2; // aumenta el tamaño del bloque
+				iteraciones.write("\nF0-F3 --> F1-F2\n");
+				//pasar a f1 y f2 de f0 y f3
+				while(f0.isEmpty()!=true||f3.isEmpty()!=true){
+					faseDos(f1,f0,f3,n,opc);
+					iteraciones.write("\nF1\n");
+					imprimirDocumento(f1,iteraciones);
+					faseDos(f2,f0,f3,n,opc);
+					iteraciones.write("\nF2\n");
+					imprimirDocumento(f2,iteraciones);
 				}
+				n*=2;
 			}
-			archivoFinal.close();
-		}catch(IOException e){
-			e.printStackTrace();
-		}
 		
+			try{
+				FileWriter archivoFinal;
+				// crear archivo ordenado, por apellido
+				if(opc=='2'){
+					archivoFinal=new FileWriter("./archivoFinalPolifase_Apellido_"+nombre+".txt");
+					System.out.println("\nTu archivo final (ordenado) está en:\n\tarchivoFinal_Apellido_"+nombre+".txt");
+				// crear archivo ordenado, por nombre
+				}else if(opc=='1'){
+					archivoFinal=new FileWriter("./archivoFinalPolifase_Nombre_"+nombre+".txt");
+					System.out.println("\nTu archivo final (ordenado) está en:\n\tarchivoFinal_Nombre_"+nombre+".txt");
+					}else
+						archivoFinal=new FileWriter("./ERROR.txt");
+				archivoFinal.write("\t***Polifase***\n");
+				// la lista completa puede quedar en f0 o f1
+				if(f0.isEmpty()!=true)
+					imprimirDocumento(f0,archivoFinal);
+				else
+					imprimirDocumento(f1,archivoFinal);
+				archivoFinal.close();
+			}catch(IOException e){
+				e.printStackTrace();
+			}
+			
 		
 			iteraciones.close();
 		}catch(IOException e){
 			e.printStackTrace();
 		}
-			/*	System.out.println("F1");
-		for(Alumno j:f1){
-			System.out.println(j.getNombre());
-			System.out.println(j.getApellido());
-			System.out.println(j.getNoCuenta());
-		}
-		System.out.println("F2");
-		for(Alumno j:f2){
-			System.out.println(j.getNombre());
-			System.out.println(j.getApellido());
-			System.out.println(j.getNoCuenta());
-		}*/
-		
-	/*	List<String> nombres=new LinkedList<>();
-		List<String> f1=new LinkedList<>();
-		List<String> f2=new LinkedList<>();
-		List<String> f3=new LinkedList<>();
-		System.out.println("polifase");
-		for(Alumno i:listadatos){
-			nombres.add(i.getNombre());
-		}
-	//  for(String i:nombres){
-	//		System.out.println(i);
-	//	}
-		int n=7,i=0,k=0;
-		String[] AOI=new String[7];
-		while(nombres.isEmpty()!=true){
-			
-			while((nombres.isEmpty()!=true)&&(i<7)){
-				//AOI[i]=nombres.get(0);
-				AOI[i]=nombres.remove(0);
-				index=AOI[i]
-				int l=i-1;
-				while(l>=0&&AOI[l]>index){
-					AOI[l+1]=AOI[l];
-					l=l-1;
+	}
+	
+	// f lista por vaciar, AOI bloque a ordenar, n tamaño del bloque
+	void insertionSort(ArrayList<Alumno> f,ArrayList<Alumno> AOI,int n,char opc){
+		Alumno index;
+		int k,i=0;
+		while(f.isEmpty()!=true&&i<n){
+			AOI.add(f.remove(0));
+			index=AOI.get(i);
+			k=i-1;
+            if(opc=='2'){
+                while(k>-1&&menor(AOI.get(k).getApellido(),index.getApellido())==1){
+					AOI.set(k+1,AOI.get(k));
+					k--;
 				}
-				AOI[l+1]=index;
-				//f1.add(nombres.remove(0));
-				i++;
+            }else{
+                while(k>-1&&menor(AOI.get(k).getNombre(),index.getNombre())==1){
+					AOI.set(k+1,AOI.get(k));
+					k--;
+				}
 			}
-			
-			for(String s:AOI)	
-			
-			
-			i=0;
-			while((nombres.isEmpty()!=true)&&(i<7)){
-				f2.add(nombres.remove(0));
-				i++;
+			AOI.set(k+1,index);
+			i++;
+		}
+	}	
+	
+	//determina cual de las dos cadenas es menor lexicograficamente
+	int menor(String nombre1,String nombre2){
+		int n,nom1,nom2,j=0,k=0;
+		if(nombre1.length()>nombre2.length())
+			n=nombre2.length();
+		else
+			n=nombre1.length();
+		int i=0;
+		do{
+			// obtiene el i caracter sin importar si es mayuscula o minuscula
+			nom1 = (int)nombre1.charAt(i)>96 ? (int)nombre1.charAt(i)-32 : (int)nombre1.charAt(i);
+			nom2 = (int)nombre2.charAt(i)>96 ? (int)nombre2.charAt(i)-32 : (int)nombre2.charAt(i);
+			i++;
+		}while(i<n&&nom1==nom2);
+		
+		if(nom1>nom2)
+			return 1;
+		else
+			return -1;
+	}
+	
+	// f -> lista donde se colocarán los bloques, fl y fr listas con claves (n tamaño del bloque)
+	void faseDos(ArrayList<Alumno> f,ArrayList<Alumno> fl,ArrayList<Alumno> fr,int n,int opc){
+		// cantidades de veces que se añadieron claves a cada lista
+		int i=0,j=0;
+		// no añadir mas de n claves por lista           
+		while((fl.isEmpty()!=true||fr.isEmpty()!=true)&&(i<n||j<n)){
+			//cuando ambos tienen aun claves por ordenar y pasar
+			if(fl.isEmpty()!=true&&fr.isEmpty()!=true&&i<n&&j<n){
+				// "desencolar" el menor de los dos bloques
+				Alumno Fl=fl.get(0);
+				Alumno Fr=fr.get(0);
+                if(opc=='2'){
+					if(menor(Fl.getApellido(),Fr.getApellido())==1){
+						f.add(fr.remove(0));
+						j++;
+					}else{
+						f.add(fl.remove(0));
+                      	i++;
+                    }
+                }else{
+					if(menor(Fl.getNombre(),Fr.getNombre())==1){
+						f.add(fr.remove(0));
+						j++;
+					}else{
+						f.add(fl.remove(0));
+						i++;
+					}
+				
+				}
+			// cuando alguna lista ya tiene sus n claves o esta vacia
+			// la otra se vacia (ya sin comparar las claves con la otra lista)
+			}else if((fl.isEmpty()==true)||(i>=n)){
+				f.add(fr.remove(0));
+				j++;
+				}else{
+					f.add(fl.remove(0));
+					i++;
+				}
+		}
+	}
+	// escribe en el archivo (iteraciones o final) los datos (Alumno) de la lista deseada
+	void imprimirDocumento(ArrayList<Alumno> f,FileWriter archivo){
+		for(Alumno alu:f){
+			try{
+				archivo.write(alu.getNombre()+", "+alu.getApellido()+", "+alu.getNoCuenta()+"\n");
+			}catch(IOException e){
+				e.printStackTrace();
 			}
-			i=0;
 		}
-		System.out.println("F1");
-		for(String j:f1){
-			System.out.println(j);
-		}
-		System.out.println("F2");
-		for(String j:f2){
-			System.out.println(j);
-		}
-		*/
 	}
 }
+
